576aa621a9d390664b6c8737aa6f34132e09418f
joyalpjose <yovellukka@gmail.com>
2023-05-20 11:15:29 +0530

    add a tab the name is fundemental data

--- dash_board.py
+++ dash_board.py
@@ -41,3 +41,25 @@ with news:
         st.write(f'Title sentiment {title_sentiment}')
         news_sentiment= df_news['sentiment_summary'][i]
         st.write(f'News sentiment {news_sentiment}')    
+
+
+
+from alpha_vantage.fundamentaldata import FundamentalData
+with fundemental_data:
+    key =''
+    fd = FundamentalData(key,output_format = 'pandas')
+    st.subheader('Balance Sheet')
+    balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
+    bs = balance_sheet.T[2:]
+    bs.columns = list(balance_sheet.T.iloc[0])
+    st.write(bs)
+    st.subheader('income Statement')
+    income_statement  = fd.get_income_statement_annual(ticker)[0]
+    is1 = income_statement.T[2:]
+    is1.columns = list(income_statement.T.iloc[0])
+    st.write(is1)
+    st.subheader('Cash Flow Statement')
+    cash_flow = fd.get_cash_flow_annual(ticker)[0]
+    cf = cash_flow.T[2:]
+    cf.columns = list(cash_flow.T.iloc[0])
+    st.write(cf)        
